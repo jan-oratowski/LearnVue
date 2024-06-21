90dc2ea15c8b09e5727c084e75a64a01a89de608
Jan Oratowski <54535207+jan-oratowski@users.noreply.github.com>
2024-06-21 20:02:40 +0200

    l5 end

--- CodePop/main.js
+++ CodePop/main.js
@@ -9,6 +9,7 @@ const app = Vue.createApp(
                 inventory: 100,
                 onSale: true,
                 details: ['50% cotton', '30% wool', '20% polyester'],
+                sizes: ['S', 'M', 'L'],
                 variants: [
                     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
                     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
