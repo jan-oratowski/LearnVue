04e451dc0987075e8bcfea51d29b1959542f9561
Jan Oratowski <54535207+jan-oratowski@users.noreply.github.com>
2024-06-21 17:03:20 +0200

    lesson2 end

--- CodePop/main.js
+++ CodePop/main.js
@@ -1,1 +1,21 @@
+const app = Vue.createApp(
+    {
+        data: function() {
+            return {
+                product: 'Socks',
+                description: 'A pair of warm, fuzzy socks',
+                image: './assets/images/socks_green.jpg',
+                inStock: true,
+                inventory: 100,
+                onSale: true,
+                details: ['50% cotton', '30% wool', '20% polyester'],
+                variants: [
+                    { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                    { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                ],
+                cart: 0
+            }
+        },
+    }
+)
 const product = 'Socks'
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 TestProj - https://learnvue.co/articles/intro-to-vue-3
+
+CodePop - https://www.youtube.com/watch?v=bzlFvd0b65c
