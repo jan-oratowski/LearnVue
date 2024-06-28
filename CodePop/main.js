ca0b278b4e0a5eb06ca57083f075af8818d77b96
Jan Oratowski <54535207+jan-oratowski@users.noreply.github.com>
2024-06-28 20:21:56 +0200

    l10 end

--- CodePop/main.js
+++ CodePop/main.js
@@ -1,11 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true,
+            cart: [],
+            premium: true
         }
     },
     methods: {
-        
-    },
+        updateCart(id) {
+            this.cart.push(id)
+        }
+    }
 })
